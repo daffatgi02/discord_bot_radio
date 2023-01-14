d9d783ad1e4844d894a57343c59ca5c0a994b982
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-14 11:32:41 +0700

    Edited index.js - Add prefix custom, semua command menggunakan prefix custom

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let prefix = "!";
+
 client.on('ready', () => {
     console.log(`Bot telah siap, dengan nama ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
         message.reply('pong');
+    } else if(command === 'prefix') {
+        if(!args.length) {
+            return message.channel.send(`Prefix saat ini adalah ${prefix}`);
+        }
+        prefix = args[0];
+        message.channel.send(`Prefix telah diganti menjadi ${prefix}`);
     }
 });
 
