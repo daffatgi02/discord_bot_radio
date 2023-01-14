31f322ebc03659e0d170f10d03412c07e2400d2d
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-14 11:24:38 +0700

    Edited index.js add command ping

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Bot telah siap, dengan nama ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
 });
 
 client.login('NzczNTgyMjM5Nzk1MTE4MTAw.GNJIFe.E-sVljdGpX_-rehkDLccmqF0nxFbOtYDMsLHk4');
